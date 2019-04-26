2ef9fd44d7582e7d2e60a74292426f9ecdac900f
Blake Walker <blake.wdesigns@gmail.com>
2019-04-26 13:27:07 -0400

    Entire Assignment

--- global.js
+++ global.js
@@ -1,9 +1,23 @@
 parseDogs()
 
 function parseDogs() {
-    fetch('https://api.thedogapi.com/v1/images/search?limit=15')
+    fetch('https://raw.githubusercontent.com/gabe1331/dog-api/master/db.json')
     .then((res) => res.json())
     .then((dogs) => {
         console.log(dogs)
+        dogs.forEach(function(dog){
+            let dogEl = `
+            <div class="card">
+                <div>
+                    <h1>${dog.breed}</h1>
+                    <img src=${dog.image}>
+                    <h3>${dog.height + ' / ' + dog.weight}</h3>
+                    <h3>${dog.lifespan}</h3>
+                    <h3>${dog.bread_for}</h3>
+                </div>
+            </div>
+        `
+        document.querySelector("#dogsWrapper").innerHTML += dogEl
+        })
     })
 }
