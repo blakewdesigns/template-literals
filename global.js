62c3762822bc8ff8627550b1571d691a7caea1a5
Gabe Wright <gabe1331@gmail.com>
2019-04-26 09:01:57 -0400

    removed forEach()

--- global.js
+++ global.js
@@ -4,9 +4,6 @@ function parseDogs() {
     fetch('https://api.thedogapi.com/v1/images/search?limit=15')
     .then((res) => res.json())
     .then((dogs) => {
-        dogs.forEach(dog => {
-            console.log(dog)
-            // your code here
-        })
+        console.log(dogs)
     })
 }
